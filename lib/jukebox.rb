10c4c891a04b67dae40e76b625c868b77a8315f5
Nate Tan <nktan93@gmail.com>
2019-03-13 21:23:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+require'pry'
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +11,62 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  range = (1..songs.length).to_a.collect do |num|
+    num.to_s
+  end
+  puts "Please enter a song name or number:"
+  loop do
+    input = gets.chomp
+    if range.include?(input)
+      puts "Playing #{songs[input.to_i - 1]}"
+      break
+    elsif songs.include?(input)
+      puts "Playing #{input}"
+      break
+    else
+      puts "Invalid input, please try again"
+      break
+    end
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "help"
+      help
+    elsif command == "play"
+      play(songs)
+    elsif command == "list"
+      list(songs)
+    elsif command == "exit"
+      exit_jukebox
+      break
+    else
+      puts "Please enter a valid command:"
+    end
+  end
+end
 
